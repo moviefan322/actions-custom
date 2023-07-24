ebcc687ad8ca1328b582660f67ec8d58b0711cca
moviefan322 <philman202@yahoo.com>
2023-07-24 12:17:01 -0400

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,8 @@ function run() {
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
 
-  core.notice("Hello from custom JS action!");
+  const websiteUrl= `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
